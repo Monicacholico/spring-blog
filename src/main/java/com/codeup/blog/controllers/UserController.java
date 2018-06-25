2bb8f95b62067522e1285031bc61b78d5f0008a4
monicacholico <monicachol@hotmail.com>
2018-06-25 09:48:46 -0500

    added style to show page

--- src/main/java/com/codeup/blog/controllers/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.codeup.blog.controllers;
-
-import com.codeup.blog.Models.User;
-import org.springframework.data.repository.CrudRepository;
-
-//public class UserController implements CrudRepository{
-//
-//
-//}User findOne(Long id);
